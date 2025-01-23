bc7f7a5579a8d7dfbd9d661e0bf89621242bad81
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-01-23 15:39:26 -0600

    Fix issues with registration and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :events, only: [ :new, :create, :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      registrations: "users/registrations",
+      sessions: "users/sessions"
+    }
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
