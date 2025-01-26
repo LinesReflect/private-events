46c3819c9df0aa0e6b0cb2d9ffc76faaf110cafc
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-01-25 20:40:21 -0600

    Refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :events, only: [ :new, :create, :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      registrations: "users/registrations",
-      sessions: "users/sessions"
-    }
-  end
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+  resources :events, only: [ :new, :create, :index ]
+  resources :users, only: [ :show ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
