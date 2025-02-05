edb2ff96108938ecdd1fac597a1538ccf25039bd
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-02-05 14:03:20 -0600

    Create scopes and update views for past, upcoming and today's events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ApplicationRecord
   has_many :attendees, through: :event_attendees,  source: :user
 
   validates :title, :location, :event_date, :details, presence: true
+
+  scope :past, -> { where("event_date < ?", DateTime.current.beginning_of_day) }
+  scope :today, -> { where(event_date: [ DateTime.current.beginning_of_day, DateTime.current.end_of_day ]) }
+  scope :upcoming, -> { where("event_date > ?", DateTime.current.end_of_day) }
 end
