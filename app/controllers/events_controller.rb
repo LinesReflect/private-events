39751a31e4f81b33c7af65f1e09d17149235c14a
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-02-06 12:38:14 -0600

    Add verify creator before action for event methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
   before_action :require_login, only: [ :new, :create, :edit, :update ]
+  before_action :verify_creator, only: [ :edit, :update ]
+  before_action
   def index
     @events = Event.all
   end
@@ -44,8 +46,15 @@ class EventsController < ApplicationController
 
   def require_login
     unless current_user
-      flash[:error] = "You must login to do this action"
+      flash[:notice] = "You must login to do this action!"
       redirect_to new_user_session_path
+    end
+  end
+
+  def verify_creator
+    unless current_user.events.include?(Event.find(params[:id]))
+      flash[:notice] = "Only the creator of this event has authority to do that action!"
+      redirect_to event_path(params[:id])
     end
   end
 end
